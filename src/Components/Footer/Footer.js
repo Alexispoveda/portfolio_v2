08dbfb1124fa3a32358fddbc28ca6f47c5b7a544
Alexis Poveda <alexispoveda97@gmail.com>
2020-12-14 09:18:37 -0500

    Fix for responsiveness

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -11,11 +11,13 @@ const Footer = () =>
         <div className="Container">
         <p>Copyright © Alexis Poveda</p>
         <p>Thanks for passing by!</p>
-        <a href="https://github.com/alexispoveda" target="blank_"><img src={githubIcon} alt="githubIcon" width="25vw"/></a>
-        <a href="https://instagram.com/alexispoveda" target="blank_"><img src={instagramIcon} alt="instagramIcon" width="25vw"/></a>
-        <a href="https://web.whatsapp.com/send?phone=50769988521" target="blank_"><img src={whatsappIcon} alt="whatsappIcon" width="25vw"/></a>
-        <a href="mailto:alexispoveda97@gmail.com"><img src={gmailIcon} alt="gmailIcon" width="25vw"/></a>
-        <a href="https://linkedin.com/in/alexispoveda" target="blank_"><img src={linkedinIcon} alt="linkedinIcon" width="25vw"/></a>
+        <div style={{display:'flex', width:'50%', alignItems:'center', justifyContent:'space-evenly'}}>
+            <a href="https://github.com/alexispoveda" target="blank_"><img src={githubIcon} alt="githubIcon" width="25vw"/></a>
+            <a href="https://instagram.com/alexispoveda" target="blank_"><img src={instagramIcon} alt="instagramIcon" width="25vw"/></a>
+            <a href="https://web.whatsapp.com/send?phone=50769988521" target="blank_"><img src={whatsappIcon} alt="whatsappIcon" width="25vw"/></a>
+            <a href="mailto:alexispoveda97@gmail.com"><img src={gmailIcon} alt="gmailIcon" width="25vw"/></a>
+            <a href="https://linkedin.com/in/alexispoveda" target="blank_"><img src={linkedinIcon} alt="linkedinIcon" width="25vw"/></a>
+        </div>
         </div>
     </footer>
 
